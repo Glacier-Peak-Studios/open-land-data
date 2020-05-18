b5cba17fa6b9c7cda2bedf85408a5e32dea5f509
Luke Mattfeld <lukemattfeld@gmail.com>
2020-05-18 11:17:00 -0700

    cleaned up err messaging

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -17,18 +18,10 @@ import (
 	"github.com/cavaliercoder/grab"
 )
 
-// type DataSource struct {
-// 	name          string
-// 	url           string
-// 	filenameInZip string
-// 	species       []string
-// 	attribution   string
-// }
-
 func main() {
-	jobs := make(chan [2]string, 1000)
-	results := make(chan string, 1000)
-	jobCount := 8
+	jobs := make(chan [2]string, 300)
+	results := make(chan string, 300)
+	jobCount := 16
 	for i := 0; i < jobCount; i++ {
 		go worker(jobs, results)
 	}
@@ -38,30 +31,34 @@ func main() {
 	for result := range results {
 		fmt.Println(result)
 	}
+	// for i := 0; i < 300; i++ {
+	// 	fmt.Println(<-results)
+	// }
 
 }
 
 func worker(jobs <-chan [2]string, results chan<- string) {
 	for job := range jobs {
-		result := ""
 		sourceDir := string(job[0])
 		fName := string(job[1])
 		fileFull := sourceDir + "/" + fName
-		result += "\n-------------------\n"
-		result += "Processing Source: " + fName + "\n"
+		logMsg(results, fName, "Processing Source")
 		err := processSource(sourceDir, fName)
 		if err != nil {
-			result += "Source failed: " + fileFull + "\n"
-			log.Print(err)
+			logMsg(results, fileFull, "Source failed!")
+			logMsg(results, fileFull, err.Error())
 		} else {
-			result += "Finished processing source: " + fName + "\n"
+			logMsg(results, fName, "Finished processing source")
 		}
-		results <- result
 	}
 }
 
+func logMsg(results chan<- string, source, msg string) {
+	toSend := source + ":: " + msg
+	results <- toSend
+}
+
 func processDirs(sourceDir string, jobs chan<- [2]string) {
-	// results :=
 	files, err := ioutil.ReadDir(sourceDir)
 	if err != nil {
 		log.Fatal(err)
@@ -73,7 +70,7 @@ func processDirs(sourceDir string, jobs chan<- [2]string) {
 			if filepath.Ext(f.Name()) == ".json" {
 				jobToAdd := [2]string{sourceDir, f.Name()}
 				jobs <- jobToAdd
-				fmt.Println("Added job:", jobToAdd[0], jobToAdd[1])
+				// fmt.Println("Added job:", jobToAdd[0], jobToAdd[1])
 			}
 
 		}
@@ -86,59 +83,55 @@ func processSource(dir string, file string) error {
 	sourceJSON := fileToStr(pathedFile)
 	sourceURL := getPropFromJSON("url", sourceJSON)
 	dlPath := strings.Replace(dir, "land-sources", "generated", 1)
-	dlFile, err := utils.DownloadFile(dlPath, sourceURL)
-	if err != nil {
-		return err
-	}
-	if filepath.Ext(dlFile) == ".zip" {
-		if !dirExists(dlPath + "/" + getFnameOnly(dlFile)) {
-			runCommand(true, "unzip", dlFile, "-d", dlPath+"/"+getFnameOnly(dlFile))
-		}
-		err := os.Remove(dlFile)
+	dlurl, _ := url.Parse(sourceURL)
+	dlFile := dlPath + "/" + filepath.Base(dlurl.Path)
+	var err error
+	// fmt.Println("Checking if dl exists: " + dlFile)
+	if !fileExists(dlFile) {
+		dlFile, err = utils.DownloadFile(dlPath, sourceURL)
 		if err != nil {
 			return err
 		}
+	} else {
+		fmt.Println("DL exists: " + dlFile)
+	}
+	if filepath.Ext(dlFile) == ".zip" {
+		if !dirExists(dlPath + "/" + getFnameOnly(dlFile)) {
+			_, err := runCommand(true, "unzip", dlFile, "-d", dlPath+"/"+getFnameOnly(dlFile))
+			if err != nil {
+				return err
+			}
+		}
+		// if fileExists(dlFile) {
+		// 	fmt.Println("Removing zip: " + dlFile)
+		// 	err := os.Remove(dlFile)
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// }
 	}
 	fname := getPropFromJSON("filenameInZip", sourceJSON)
 	switch filetype := getPropFromJSON("filetype", sourceJSON); filetype {
 	case "geojson":
-		processGeoJSON(dlFile, fname)
+		return processGeoJSON(dlPath, getFnameOnly(dlFile))
 	case "shp":
-		processShp(dlPath+"/"+getFnameOnly(dlFile), fname)
+		return processShp(dlPath+"/"+getFnameOnly(dlFile), fname, getFnameOnly(file))
 	}
 
 	return nil
 }
 
-func processGeoJSON(path string, filename string) error {
-	fmt.Println("Processing geoJson: " + path + ", " + filename)
-	return nil
-}
-
-func processShp(path string, filename string) error {
-	if filename == "" {
-		shapefiles, err := utils.WalkMatch(path, "*.shp")
-		if err != nil {
-			return err
-		}
-		if len(shapefiles) > 1 {
-			return errors.New("Multiple shapefiles in zip + none specified")
-		}
-		filename = filepath.Base(shapefiles[0])
-	}
-	// basepath := filepath.Dir(path)
+func processGeoJSON(path, filename string) error {
 	filename = getFnameOnly(filename)
 	fileWithPath := path + "/" + filename
 	geojson := fileWithPath + ".geojson"
-	shapefile := fileWithPath + ".shp"
 	labels := fileWithPath + "-labels.geojson"
-	// fmt.Println(geojson + ", " + shapefile)
+	fmt.Println("Processing geoJson: " + geojson)
 
-	_, err := runCommand(true, "ogr2ogr", "-f", "GeoJSON", "-t_srs", "crs:84", geojson, shapefile)
+	err := runAndWriteCommand(labels, "geojson-polygon-labels", "--label=polylabel", "--include-minzoom=6-11", geojson)
 	if err != nil {
 		return err
 	}
-	runAndWriteCommand(labels, "geojson-polygon-labels", "--label=polylabel", "--include-minzoom=6-11", geojson)
 	_, err = runCommand(false, "tippecanoe", "-z11", "-o", fileWithPath+".mbtiles", geojson)
 	if err != nil {
 		return err
@@ -151,6 +144,34 @@ func processShp(path string, filename string) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func processShp(path, filename, fileOutName string) error {
+	if filename == "" {
+		shapefiles, err := utils.WalkMatch(path, "*.shp")
+		if err != nil {
+			return err
+		}
+		if len(shapefiles) > 1 {
+			return errors.New("Multiple shapefiles in zip, none specified")
+		}
+		filename = filepath.Base(shapefiles[0])
+	}
+	basepath := filepath.Dir(path)
+	filename = getFnameOnly(filename)
+	fileOutName = getFnameOnly(fileOutName)
+	fileWithPath := path + "/" + filename
+	geojson := basepath + "/" + fileOutName + ".geojson"
+	shapefile := fileWithPath + ".shp"
+	// fmt.Println(geojson + ", " + shapefile)
+	fmt.Println("Processing shapefile: " + shapefile)
+
+	_, err := runCommand(true, "ogr2ogr", "-f", "GeoJSON", "-t_srs", "crs:84", geojson, shapefile)
+	if err != nil {
+		return err
+	}
+	err = processGeoJSON(basepath, fileOutName)
 
 	return nil
 }
@@ -217,8 +238,11 @@ func getPropFromJSON(prop string, strJSON string) string {
 	return ""
 }
 
-func runAndWriteCommand(outName, cmdName string, args ...string) {
-	fmt.Println("Running cmd: " + cmdName + " " + strings.Join(args, " ") + " > " + outName)
+func runAndWriteCommand(outName, cmdName string, args ...string) error {
+	// for i := 0; i < len(args); i++ {
+	// 	args[i] = strings.Replace(args[i], " ", "\\ ", -1)
+	// }
+	// fmt.Println("Running cmd: " + cmdName + " " + strings.Join(args, " ") + " > " + outName)
 	cmd := exec.Command(cmdName, args...)
 
 	outfile, err := os.Create(outName)
@@ -230,12 +254,17 @@ func runAndWriteCommand(outName, cmdName string, args ...string) {
 
 	err = cmd.Start()
 	if err != nil {
-		panic(err)
+		fmt.Println("Error running cmd: " + cmdName + " " + strings.Join(args, " ") + " > " + outName)
+		return err
 	}
 	cmd.Wait()
+	return nil
 }
 
 func runCommand(silent bool, cmd string, args ...string) (string, error) {
+	// for i := 0; i < len(args); i++ {
+	// 	args[i] = strings.Replace(args[i], " ", "\\ ", -1)
+	// }
 	// fmt.Println("Running cmd: " + cmd + " " + strings.Join(args, " "))
 	out, err := exec.Command(cmd, args...).Output()
 	if err != nil {
